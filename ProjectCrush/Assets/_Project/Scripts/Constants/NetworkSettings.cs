db84399a2aeb528d5037e88e62b73c2b8f825d1d
Unknown <ssmith37@uccs.edu>
2020-05-20 13:09:36 -0600

    Adding basic menu & process to connect to Photon Network.

---
+++ ProjectCrush/Assets/_Project/Scripts/Constants/NetworkSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class NetworkSettings
+{
+	public const string GAME_VERSION = "0.0.0.1";
+
+	public const int MAX_PLAYER_COUNT = 4;
+}
