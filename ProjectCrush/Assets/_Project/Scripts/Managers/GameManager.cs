4f1229e32b530df5b1ca88a4a7176df6af7c20df
Unknown <ssmith37@uccs.edu>
2020-05-22 00:42:10 -0600

    Adjusting offset spawn for game manager process to separate players on load.

--- ProjectCrush/Assets/_Project/Scripts/Managers/GameManager.cs
+++ ProjectCrush/Assets/_Project/Scripts/Managers/GameManager.cs
@@ -2,12 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : Photon.PunBehaviour
+public class GameManager : Photon.PunBehaviour, IPunObservable
 {
 	[SerializeField]
 	private GameObject playerPrefab;
+	int tick = 0;
+
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			stream.SendNext(tick);
+		}
+		else
+		{
+			tick = (int)stream.ReceiveNext();
+		}
+	}
+
 	void Start()
 	{
-		PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0, 0, 0f), Quaternion.identity, 0);
+		PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(Random.Range(-5f, 5f), 0, Random.Range(-5f, 5f)).normalized * 5f, Quaternion.identity, 0);
+		tick++;
 	}
 }
